0b52ed4396bc47dfcc473cadc9d9fbe5a4cffae3
Henrique Luiz <heenluy@gmail.com>
2023-01-30 15:39:36 -0300

    chore(login): adds component router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/containers/login/login.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', component: LoginPage, title: 'Dashboard — Entrar no painel administrativo' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
