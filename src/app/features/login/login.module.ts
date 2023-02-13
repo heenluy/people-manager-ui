35f55a0259d1f73038246d7b8ddc45e84e0c26bc
Henrique Luiz <heenluy@gmail.com>
2023-02-13 16:54:18 -0300

    chore: updates login component

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms'
 
-import { LoginPage } from './containers/login/login.page';
+import { LoginPage } from './login/login.page';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 
