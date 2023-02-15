27c2911a04d64ca705e0a1521518decf80caf26d
Henrique Luiz <heenluy@gmail.com>
2023-02-15 12:20:38 -0300

    chore: adds delete-person component

---
+++ src/app/features/dashboard/delete-person/delete-person.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-person',
+  templateUrl: './delete-person.component.html',
+  styleUrls: ['./delete-person.component.scss']
+})
+export class DeletePersonComponent {
+
+}
